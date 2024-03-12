2ca54361271fa14a4f4ca3575e0152eda7369bc6
mrtyazilim <bt.mrt@yandex.com>
2024-03-12 09:53:53 +0300

    ilk add , ilk push

---
+++ src/Transaction.java
@@ -0,0 +1,17 @@
+/*
+burayı bir paket açıp da  içine koymadım. belki de kötü yaptım
+
+boşluklu enum yok galiba
+*/
+public class Transaction {
+
+
+    /*
+    enum description{
+        Avans çekildi,
+
+    }
+
+     */
+
+}
