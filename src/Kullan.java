5efc1f72e239b8c554ee1af5d64ba7670b04686a
mrtyazilim <bt.mrt@yandex.com>
2024-03-19 08:10:21 +0300

    githubcomda şube seçili

--- src/Kullan.java
+++ src/Kullan.java
@@ -1,8 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 class Kullan {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
-        System.out.println(today);
+//        System.out.println(today);
+        //LocalTime localTime=new LocalTime();
 
+        today.;
     }
 }
